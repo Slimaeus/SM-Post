ebfaa5599134462c22cbf22e35e7cf160c2bc595
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-01-22 17:22:29 +0700

    Add IEventStore, EventStore, MongoDbConfig

---
+++ Post.Cmd.Infrastructure/Stores/EventStore.cs
@@ -0,0 +1,24 @@
+﻿using CQRS.Core.Domain;
+using CQRS.Core.Events;
+using CQRS.Core.Infrastructure;
+
+namespace Post.Cmd.Infrastructure.Stores
+{
+    public class EventStore : IEventStore
+    {
+        private readonly IEventStoreRepository _eventStoreRepository;
+        public EventStore(IEventStoreRepository eventStoreRepository)
+        {
+            _eventStoreRepository = eventStoreRepository;
+        }
+        public Task<List<BaseEvent>> GetEventsAsync(Guid aggregateId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveEventAsync(Guid aggregateId, IEnumerable<BaseEvent> events, int expectedVersion)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
