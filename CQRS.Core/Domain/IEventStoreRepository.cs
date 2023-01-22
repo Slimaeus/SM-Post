4c8d7d35609c9ca54fbc76d2e9dd5a3e78279aaf
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-01-22 17:06:19 +0700

    Add IEventStoreRepository

---
+++ CQRS.Core/Domain/IEventStoreRepository.cs
@@ -0,0 +1,10 @@
+﻿using CQRS.Core.Events;
+
+namespace CQRS.Core.Domain
+{
+    public interface IEventStoreRepository
+    {
+        Task SendAsync(EventModel @event);
+        Task<List<EventModel>> FindByAggredateId(Guid aggregateId);
+    }
+}
