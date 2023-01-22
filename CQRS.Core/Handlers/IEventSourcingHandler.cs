24aeafd35ba40f6ee5b57a422bf740e956b677de
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-01-23 06:35:48 +0700

    Add IEventSourcingHandler

---
+++ CQRS.Core/Handlers/IEventSourcingHandler.cs
@@ -0,0 +1,10 @@
+﻿using CQRS.Core.Domain;
+
+namespace CQRS.Core.Handlers
+{
+    public interface IEventSourcingHandler<T>
+    {
+        Task SendAsync(AggregateRoot aggregate);
+        Task<T> GetByIdAsync(Guid id);
+    }
+}
