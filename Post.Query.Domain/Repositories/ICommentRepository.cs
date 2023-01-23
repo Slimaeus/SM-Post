191fa71c6791736305ded5c23489f0316a289943
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-01-23 09:04:16 +0700

    Added ICommentRepository

---
+++ Post.Query.Domain/Repositories/ICommentRepository.cs
@@ -0,0 +1,12 @@
+﻿using Post.Query.Domain.Enitities;
+
+namespace Post.Query.Domain.Repositories
+{
+    public interface ICommentRepository
+    {
+        Task CreateAsync(CommentEntity comment);
+        Task UpdateAsync(CommentEntity comment);
+        Task<CommentEntity> GetByIdAsync(Guid commentId);
+        Task DeleteAsync(Guid commentId);
+    }
+}
