b49c39f458d025651f9cd0be994f473211ac4efb
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-01-23 10:58:04 +0700

    Added EventConsumer

--- Post.Query.Api/Program.cs
+++ Post.Query.Api/Program.cs
@@ -1,5 +1,8 @@
+using Confluent.Kafka;
+using CQRS.Core.Consumers;
 using Microsoft.EntityFrameworkCore;
 using Post.Query.Domain.Repositories;
+using Post.Query.Infrastructure.Consumers;
 using Post.Query.Infrastructure.DataAccess;
 using Post.Query.Infrastructure.Handlers;
 using Post.Query.Infrastructure.Repositories;
@@ -18,6 +21,8 @@ dataContext.Database.EnsureCreated();
 builder.Services.AddScoped<IPostRepository, PostRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 builder.Services.AddScoped<IEventHandler, Post.Query.Infrastructure.Handlers.EventHandler>();
+builder.Services.Configure<ConsumerConfig>(builder.Configuration.GetSection(nameof(ConsumerConfig)));
+builder.Services.AddScoped<IEventConsumer, EventConsumer>();
 
 builder.Services.AddControllers();
 
---
+++ Post.Query.Infrastructure/Consumers/EventConsumer.cs
@@ -0,0 +1,61 @@
+﻿using Confluent.Kafka;
+using CQRS.Core.Consumers;
+using CQRS.Core.Events;
+using Microsoft.Extensions.Options;
+using Post.Query.Infrastructure.Converters;
+using Post.Query.Infrastructure.Handlers;
+using System.Text.Json;
+
+namespace Post.Query.Infrastructure.Consumers
+{
+    public class EventConsumer : IEventConsumer
+    {
+        private readonly ConsumerConfig _config;
+        private readonly IEventHandler _eventHandler;
+
+        public EventConsumer(IOptions<ConsumerConfig> config, IEventHandler eventHandler)
+        {
+            _config = config.Value;
+            _eventHandler = eventHandler;
+        }
+
+        public void Consume(string topic)
+        {
+            using var consumer = new ConsumerBuilder<string, string>(_config)
+                .SetKeyDeserializer(Deserializers.Utf8)
+                .SetValueDeserializer(Deserializers.Utf8)
+                .Build();
+
+            consumer.Subscribe(topic);
+
+            while (true)
+            {
+                var consumerResult = consumer.Consume();
+
+                if (consumerResult?.Message == null)
+                {
+                    continue;
+                }
+
+                var options = new JsonSerializerOptions
+                {
+                    Converters =
+                    {
+                        new EventJsonConverter()
+                    }
+                };
+                var @event = JsonSerializer.Deserialize<BaseEvent>(consumerResult.Message.Value, options);
+                var handleMethod = _eventHandler.GetType().GetMethod("On", new Type[] { @event.GetType() });
+
+                if (handleMethod == null)
+                {
+                    throw new ArgumentNullException(nameof(handleMethod), "Could not find event handler method!");
+                }
+
+                handleMethod.Invoke(_eventHandler, new object[] { @event });
+                consumer.Commit(consumerResult);
+            }
+
+        }
+    }
+}
