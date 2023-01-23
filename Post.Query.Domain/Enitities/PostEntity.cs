26b8cc1d3ae28feb9970d8646ddd2d1ffd2bcc48
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-01-23 08:57:20 +0700

    Update PostEntity

--- Post.Query.Domain/Enitities/PostEntity.cs
+++ Post.Query.Domain/Enitities/PostEntity.cs
@@ -9,5 +9,9 @@ namespace Post.Query.Domain.Enitities
         [Key]
         public Guid PostId { get; set; }
         public string Author { get; set; }
+        public DateTime DatePosted { get; set; }
+        public string Message { get; set; }
+        public int Likes { get; set; }
+        public virtual ICollection<CommentEntity> Comments { get; set; }
     }
 }
