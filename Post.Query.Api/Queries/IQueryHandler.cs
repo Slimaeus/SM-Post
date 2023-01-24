9e3a5e979c730314a261f2f48f6e0abf32e8592d
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-01-24 19:29:03 +0700

    Added IQueryHandler

---
+++ Post.Query.Api/Queries/IQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using Post.Query.Domain.Enitities;
+
+namespace Post.Query.Api.Queries
+{
+    public interface IQueryHandler
+    {
+        Task<List<PostEntity>> HandleAsync(FindAllPostsQuery query);
+        Task<List<PostEntity>> HandleAsync(FindPostByIdQuery query);
+        Task<List<PostEntity>> HandleAsync(FindPostsByAuthorQuery query);
+        Task<List<PostEntity>> HandleAsync(FindPostsWithCommentsQuery query);
+        Task<List<PostEntity>> HandleAsync(FindPostsWithLikesQuery query);
+    }
+}
